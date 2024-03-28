58a350dc88070947fe8ab89017940b74e25ea720
Corentin <corentin.henrion6@gmail.com>
2024-03-28 15:46:07 +0100

    maj

--- meteo.py
+++ meteo.py
@@ -9,7 +9,30 @@ def get_weather(lat, lon):
     return data
 
 if __name__ == "__main__":
-    latitude = float(os.getenv('LAT'))
-    longitude = float(os.getenv('LONG'))
-    weather_data = get_weather(latitude, longitude)
-    print(weather_data)
+    lat_str = os.getenv('LAT')
+    lon_str = os.getenv('LONG')
+
+    if lat_str is not None and lon_str is not None:
+        try:
+            latitude = float(lat_str)
+            longitude = float(lon_str)
+
+            weather_data = get_weather(latitude, longitude)
+            
+            coord = weather_data['coord']
+            weather = weather_data['weather'][0]
+            main = weather_data['main']
+            wind = weather_data['wind']
+            sys = weather_data['sys']
+
+            print(f"Coordonnées : longitude {coord['lon']}, latitude {coord['lat']}")
+            print(f"Météo : {weather['description']}")
+            print(f"Température : {main['temp']} Kelvin")
+            print(f"Vitesse du vent : {wind['speed']} m/s")
+            print(f"Pression atmosphérique : {main['pressure']} hPa")
+            print(f"Humidité : {main['humidity']}%")
+            print(f"Pays : {sys['country']}")
+        except ValueError:
+            print("Les valeurs de latitude et de longitude ne sont pas valides.")
+    else:
+        print("Les variables d'environnement LAT et LONG ne sont pas définies.")
