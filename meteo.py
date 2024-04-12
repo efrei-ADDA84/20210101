3414010e81edf5cfee8af5f380dd6dc3dc86ad4a
Corentin <corentin.henrion6@gmail.com>
2024-04-12 17:04:18 +0200

    modif dockerfile6

--- meteo.py
+++ meteo.py
@@ -49,4 +49,4 @@ def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
-    app.run(host='0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
